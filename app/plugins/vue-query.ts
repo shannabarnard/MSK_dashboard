e8092377dd4a76dcf5e55f051e894fcfc03fc399
Shanna Barnard <shannabarnard@gmail.com>
2026-05-06 18:39:40 +0100

    chore(tanstack): add and register vue-query

---
+++ app/plugins/vue-query.ts
@@ -0,0 +1,17 @@
+import { QueryClient, VueQueryPlugin } from "@tanstack/vue-query";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 30_000,
+        retry: 1,
+        refetchOnWindowFocus: false
+      }
+    }
+  });
+
+  nuxtApp.vueApp.use(VueQueryPlugin, {
+    queryClient
+  });
+});
